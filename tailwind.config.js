00dff43d5d29633e4596ff2de7c846a013e091e6
mistermathindonesia <123525116+mistermathindonesia@users.noreply.github.com>
2023-10-09 17:22:36 +0700

    create pages and routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    // mendeteksi semua classes yg ditambahkan, any tailwind css class. you add is going to be detected and then css that equivalent to that has been cretaed
+    // mendeteksi semua classes yg ditambahkan, any tailwind css class. you add is going to be detected and then css that equivalent to that has been created
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
